d1737f9b405c090dbd8829e0b1533462e2493b75
Greg Parr <greg.parr@blueyonder.co.uk>
2022-06-18 21:27:13 +0100

    initial commit

---
+++ schemas/album.js
@@ -0,0 +1,32 @@
+export default {
+  name: "album",
+  type: "document",
+  title: "Album",
+  fields: [
+    {
+      name: "gameId",
+      type: "number",
+      title: "Game ID",
+    },
+    {
+      name: "artist",
+      type: "string",
+      title: "Artist",
+    },
+    {
+      name: "albumTitle",
+      type: "string",
+      title: "Album Title",
+    },
+    {
+      name: "embedKey",
+      type: "string",
+      title: "Embed Key",
+    },
+    {
+      name: "coverArt",
+      type: "image",
+      title: "Cover Art",
+    },
+  ],
+};
