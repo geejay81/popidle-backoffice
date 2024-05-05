a2a808f9ca34366927cf12004d78bc001f627b56
Greg Parr <greg.parr@blueyonder.co.uk>
2024-05-05 16:21:01 +0100

    created new v3 project

--- sanity.cli.js
+++
@@ -1,9 +0,0 @@
-// sanity.cli.js
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'v6qz2c5y',
-    dataset: 'production'
-  }
-})
--- schemas/schema.js
+++
@@ -1,9 +0,0 @@
-import album from './album'
-import movie from './movie'
-import single from './single'
-
-export default [
-  album,
-  movie,
-  single
-]
