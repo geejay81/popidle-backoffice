297a8c8cee4d9d6bc80c7f1b55ec654f698eea7c
Greg Parr <greg.parr@blueyonder.co.uk>
2024-06-03 11:04:25 +0100

    updated movie schema to include additional properties

--- schemaTypes/movie.js
+++ schemaTypes/movie.js
@@ -9,19 +9,47 @@ export default {
         title: "Game ID",
       },
       {
+        name: "decadeGameId",
+        type: "number",
+        title: "Decade Game ID",
+      },
+      {
         name: "title",
         type: "string",
         title: "Title",
       },
       {
+        name: "tagline",
+        type: "string",
+        title: "Tagline",
+      },
+      {
+        name: "overview",
+        description: "Short synopsis of the movie",
+        type: "text",
+        title: "Overview",
+      },
+      {
         name: "year",
+        description: "Year the movie was released",
         type: "number",
         title: "Year Released",
       },
       {
-        name: "embedKey",
+        name: "releaseDate",
+        description: "The date the movie was released",
+        type: "date",
+        title: "Release date",
+      },
+      {
+        name: "imdbId",
         type: "string",
-        title: "Embed Key",
+        title: "IMDb Id",
+      },
+      {
+        name: "tmdbId",
+        type: "string",
+        title: "TMDB Id",
       },
       {
         name: "poster",
@@ -29,9 +57,32 @@ export default {
         title: "Poster",
       },
       {
-        name: "decadeGameId",
+        name: "budget",
         type: "number",
-        title: "Decade Game ID",
+        title: "Budget",
+      },
+      {
+        name: "revenue",
+        type: "number",
+        title: "Revenue",
+      },
+      {
+        name: "runtime",
+        type: "number",
+        title: "Runtime",
+        description: "Runtime in minutes",
+      },
+      {
+        name: 'genres',
+        type: 'array',
+        of: [{type: 'string'}],
+        title: 'Genres',
+      },
+      {
+        name: 'rating',
+        type: 'number',
+        title: 'Rating',
+        description: 'Average rating out of 10'
       },
     ],
     preview: {
